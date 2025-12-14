dac5d8beafd9c39a0c03ec1f41af26a8b0a0cf62
kyo0221 <s21c1135sc@s.chibakoudai.jp>
2025-12-14 20:05:34 +0900

    ✨feat: add chassis and can

--- main_executor/src/main.cpp
+++ main_executor/src/main.cpp
@@ -1,4 +1,6 @@
 #include <rclcpp/rclcpp.hpp>
+#include "chassis_driver/chassis_driver_node.hpp"
+#include "socketcan_interface/socketcan_interface_node.hpp"
 #include "onigokko/onigokko_node.hpp"
 #include "teleop_omni/teleop_omni_node.hpp"
 
@@ -12,7 +14,11 @@ int main(int argc, char * argv[]){
 
     auto onigokko_node = std::make_shared<onigokko::OnigokkoNode>(nodes_option);
     auto teleop_omni_node = std::make_shared<teleop_omni::TeleopOmniNode>(nodes_option);
+    auto socketcan_node = std::make_shared<socketcan_interface::SocketcanInterface>(nodes_option);
+    auto chassis_driver_node = std::make_shared<chassis_driver::ChassisDriver>(nodes_option);
 
+    exec.add_node(socketcan_node);
+    exec.add_node(chassis_driver_node);
     exec.add_node(onigokko_node);
     exec.add_node(teleop_omni_node);
 
