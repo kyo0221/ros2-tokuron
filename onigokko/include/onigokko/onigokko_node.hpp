41161b1575b32f398f906586fe4c23d7164666ae
TakumiIshiguro <aquarius1496@gmail.com>
2025-12-15 16:40:49 +0900

    add autonomous topic

--- onigokko/include/onigokko/onigokko_node.hpp
+++ onigokko/include/onigokko/onigokko_node.hpp
@@ -14,7 +14,17 @@ public:
     explicit OnigokkoNode(const std::string& name_space, const rclcpp::NodeOptions& options = rclcpp::NodeOptions());
 
 private:
-    const double dummy_parameter;
+    void cloudCallback(const sensor_msgs::msg::PointCloud2::SharedPtr msg);
+    void ComputeVel(const sensor_msgs::msg::PointCloud2 & msg, geometry_msgs)::
+    
+    rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr cloud_sub_;
+    rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr cmd_vel_pub_;
+    rclcpp::Publisher<std_msgs::msg::Bool>::SharedPtr autonomous_pub_;
+
+    const double max_vel_;
+    const double ignore_range_;
+    bool autonomous_flag_;
+
     rclcpp::QoS _qos;
 };
 
--- onigokko/src/onigokko_node.cpp
+++ onigokko/src/onigokko_node.cpp
@@ -9,6 +9,7 @@ OnigokkoNode::OnigokkoNode(const std::string& name_space, const rclcpp::NodeOpti
 this->get_parameter("cloud_topic").as_string(),
 max_vel_(this->get_parameter("max_vel").as_double()),
 ignore_range_(this->get_parameter("ignore_range").as_double()),
+autonomous_flag_(false),
 _qos(rclcpp::QoS(10))
 {
     cloud_sub_ = this->create_subscription<sensor_msgs::msg::PointCloud2>(
@@ -17,19 +18,27 @@ _qos(rclcpp::QoS(10))
     );
 
     cmd_vel_pub_ = this->create_publisher<geometry_msgs::msg::Twist>("cmd_vel" _qos);
+    autonomous_flag_ = this->create_publisher<std_msgs::msg::Bool>("autonomous" _qos);
+
+    auto autonomous_msg = std_msgs::msg::Bool();
+    autonomous_msg.data = autonomous_flag_;
+    autonomous_pub_->publish(autonomous_msg);
+
     RCLCPP_INFO(this->get_logger(), "Onigokko Node has been started. dummy_parameter: %.2f");
 }
 
 void OnigokkoNode::cloudCallback(const sensor_msgs::msg::PointCloud2::SharedPtr msg)
 {
     geometry_msgs::msg::Twist twist_msg;
-    if (ComputeVel(*msg, twist_msg)) {
+    if (autonomous_flag_ == true){
+        ComputeVel(*msg, twist_msg)
         cmd_vel_pub_->publish(twist_msg);
     } else {
-        cmd_vel_pub_->publish(geometry_msgs::msg::Twist());
+        return;
     }
 }
 
-void OnigokkoNode::ComputeVel(
-    const sensor_msgs::msg::PointCloud2 & msg,
-    geometry_msgs::msg::Twist & out_vel)
+void OnigokkoNode::ComputeVel(const sensor_msgs::msg::PointCloud2 & msg, geometry_msgs::msg::Twist & out_vel)
+{
+    
+}
